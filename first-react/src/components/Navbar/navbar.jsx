282fcebf2ec5ebbd7e891044cf45e12049be8947
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-02 14:08:43 +0530

    Navbar component added.

---
+++ first-react/src/components/Navbar/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
