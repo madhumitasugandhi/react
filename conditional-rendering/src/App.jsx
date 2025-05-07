cae1ba1173ba33287964fb8cab0154ab590cb4c7
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-07 14:07:46 +0530

    App.jsx file updated by using logical operator in conditional rendering.

--- conditional-rendering/src/App.jsx
+++ conditional-rendering/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LogoutBtn from './components/LogoutBtn';
+import LoginBtn from './components/LoginBtn';
 
 function App() {
-
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   return (
-      <div>
-
-      </div>
-    
+  <div>
+   <h1>
+    Welcome to my app
+    <div>
+      {isLoggedIn && <LogoutBtn /> }
+    </div>
+   </h1>
+  </div>
   )
 }
-
 export default App
