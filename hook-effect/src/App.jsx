fc421179c84f4341ee7345676bfa72dbaa418350
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-08 13:46:44 +0530

    Contact us added.

--- hook-effect/src/App.jsx
+++ hook-effect/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+ 
+  useEffect(() => {
+    alert("I wil run on only 1st render");
+    }, [])
+  
+    function handleClick() {
+      setCount(count + 1);
+    }
 
   return (
       <div>
-        
+        <button onClick={handleClick}>
+          Click me
+        </button>
+        <br/>
+        Count is: {count}
+        <br/>
       </div>
   )
 }
