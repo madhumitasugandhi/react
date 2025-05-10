e9244d98a42e087efea65fdc9d8a788b691f52d1
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-10 22:39:09 +0530

    Home page updated.

--- react-router-project/src/components/Home.jsx
+++ react-router-project/src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate('/about');
+  }
+
   return (
-    <div>Home Page</div>
+    <div>
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>
+        Move to About Page
+      </button>
+    </div>
+
   )
 }
 
