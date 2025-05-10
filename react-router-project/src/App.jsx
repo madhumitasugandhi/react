73afadbe9911d940b045276013f46b64272f41c9
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-10 22:12:24 +0530

    ParamComp route added.

--- react-router-project/src/App.jsx
+++ react-router-project/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ParamComp from './components/ParamComp';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: 
+      <div>
+        <Navbar />
+        <Home />
+      </div>
+    },
+    {
+      path: '/about',
+      element: <div>
+        <Navbar />
+        <About />
+      </div>
+    },
+    {
+      path: '/dashboard',
+      element: <div>
+        <Navbar />
+        <Dashboard />
+      </div>
+    },
+    {
+      path: "/student/:id",
+      element: <div>
+        <Navbar />
+        <ParamComp/>
+        <h1>Student</h1>
+      </div>
+    }
+  ]
+);
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
+
   )
 }
 
