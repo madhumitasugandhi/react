aa0ebb57d19b01d6042f1c2805496a408d0a6948
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-10 22:26:24 +0530

    Dashboard page updated.

--- react-router-project/src/components/Dashboard.jsx
+++ react-router-project/src/components/Dashboard.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate('/');
+  }
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2>Dashboard Page</h2>
+      <button onClick={handleClick}>
+        Move to Home Page
+      </button>
+    </div>
+
   )
 }
-
+  
 export default Dashboard
