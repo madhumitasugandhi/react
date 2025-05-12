aeb404cf110b33fcdcbe4198a2d1cba879203929
madhumitasugandhi <madhumitasugandhi@gmail.com>
2025-05-12 14:08:47 +0530

    Forms tag added validation added in App.jsx File.

--- react-form-project/src/App.jsx
+++ react-form-project/src/App.jsx
@@ -7,28 +7,52 @@ function App() {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm("Submission");
 
-  function onSubmit(data) {
-    console.log(data);
-  }
+   async function onSubmit(data) { 
+
+    await new Promise((resolve) => setTimeout(resolve, 2000)); 
+    console.log( "Submitting the form", data); 
+    alert("Form submitted successfully");
+    }
+  
+
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label >Fisrt name:</label>
-        <input {...register('firstName')}/>
+        <input {...register('firstName', {
+          required: true,
+          minLength: { value: 3, message: "Minimum length is 3" },
+          maxLength: { value: 10, message: "Maximum length is 10" },
+           pattern: {
+            value: /^[A-Za-z]+$/i,
+            message: "Only letters are allowed"
+          }
+        },
+        )} />
+        {errors.firstName && <p className='error-msg'>{errors.firstName.message}</p>}
       </div>
       <div>
         <label >Last name:</label>
-        <input {...register('lastName')}/>
+        <input {...register('lastName',{
+          required: true,
+          minLength: { value: 3, message: "Minimum length is 3" },
+          maxLength: { value: 10, message: "Maximum length is 10" },
+           pattern: {
+            value: /^[A-Za-z]+$/i,
+            message: "Only letters are allowed"
+          }
+        },)} />
+        {errors.lastName && <p className='error-msg'>{errors.lastName.message}</p>}
       </div>
       <div>
         <label >Email:</label>
-        <input {...register('email')}/>
+        <input {...register('email')} />
       </div>
-      <br/>
-      <input type="submit" />
+      <br />
+      <input type="submit" disabled={isSubmitting} value={isSubmitting ? "Submitting": "Submit" }/>
     </form>
   )
 }
